ed0c9adfd8194ae6137cf0813f830b3baa42905c
Andrew Lowe <doodlefolks@gmail.com>
2016-02-21 21:56:48 -0800

    Add flashing checkerboard

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 // Your JS goes here
-var body = document.getElementsByTagName('body')[0];
-for (var i = 1; i <= 9; i++) {
-  for (var j = 1; j <= 9; j++) {
-    var newSquare = document.createElement('div');
-    newSquare.style.width = '11.1%';
-    newSquare.style.paddingBottom = '11.1%';
-    newSquare.style.cssFloat = 'left';
-    var red = Math.round(Math.random() * 255);
-    var blue = Math.round(Math.random() * 255);
-    var green = Math.round(Math.random() * 255);
-    newSquare.style.background = `rgb(${red}, ${green}, ${blue})`;
-    body.appendChild(newSquare);
+var flash = function() {
+  var body = document.getElementsByTagName('body')[0];
+  for (var i = 0; i < body.children.length; i++) {
+    if (body.children[i].tagName = 'DIV') {
+      body.removeChild(body.children[i]);
+      i--;
+    }
   }
-}
+  for (var i = 1; i <= 9; i++) {
+    for (var j = 1; j <= 9; j++) {
+      var newSquare = document.createElement('div');
+      newSquare.style.width = '11.1%';
+      newSquare.style.paddingBottom = '11.1%';
+      newSquare.style.cssFloat = 'left';
+      var red = Math.round(Math.random() * 255);
+      var blue = Math.round(Math.random() * 255);
+      var green = Math.round(Math.random() * 255);
+      newSquare.style.background = `rgb(${red}, ${green}, ${blue})`;
+      body.appendChild(newSquare);
+    }
+  }
+};
+flash();
+window.setInterval(flash, 2000);
