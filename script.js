157b61f3bed0d48c71f36727d69a066943130554
Andrew Lowe <doodlefolks@gmail.com>
2016-02-21 17:48:03 -0800

    Add standard checkerboard js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Your JS goes here
+var body = document.getElementsByTagName('body')[0];
+for (var i = 1; i <= 9; i++) {
+  for (var j = 1; j <= 9; j++) {
+    var newSquare = document.createElement('div');
+    newSquare.style.width = '11.1%';
+    newSquare.style.paddingBottom = '11.1%';
+    newSquare.style.cssFloat = 'left';
+    if ((i % 2 == 1 && j % 2 == 1) || (i % 2 == 0 && j % 2 == 0)) {
+      newSquare.style.background = 'black';
+    } else {
+      newSquare.style.background = 'red';
+    }
+    body.appendChild(newSquare);
+  }
+}
