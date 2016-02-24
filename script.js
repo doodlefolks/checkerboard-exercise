21936ee139e239141a8f2e0732265a456be00890
Andrew Lowe <doodlefolks@gmail.com>
2016-02-24 13:37:16 -0800

    Lint flashing branch

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
-// Your JS goes here
-var flash = function() {
-  var body = document.getElementsByTagName('body')[0];
-  body.innerHTML = '';
-  for (var i = 1; i <= 9; i++) {
-    for (var j = 1; j <= 9; j++) {
-      var newSquare = document.createElement('div');
-      newSquare.style.width = '11.1%';
-      newSquare.style.paddingBottom = '11.1%';
-      newSquare.style.cssFloat = 'left';
-      var red = Math.round(Math.random() * 255);
-      var blue = Math.round(Math.random() * 255);
-      var green = Math.round(Math.random() * 255);
-      newSquare.style.background = `rgb(${red}, ${green}, ${blue})`;
-      body.appendChild(newSquare);
+(function() {
+  'use strict';
+  var flash;
+
+  flash = function() {
+    var body;
+    var newSquare;
+    var red;
+    var blue;
+    var green;
+
+    body = document.getElementsByTagName('body')[0];
+    body.innerHTML = '';
+    for (var i = 1; i <= 9; i++) {
+      for (var j = 1; j <= 9; j++) {
+        newSquare = document.createElement('div');
+        newSquare.style.width = '11.1%';
+        newSquare.style.paddingBottom = '11.1%';
+        newSquare.style.cssFloat = 'left';
+        red = Math.round(Math.random() * 255);
+        blue = Math.round(Math.random() * 255);
+        green = Math.round(Math.random() * 255);
+        newSquare.style.background = `rgb(${red}, ${green}, ${blue})`;
+        body.appendChild(newSquare);
+      }
     }
-  }
-};
-flash();
-window.setInterval(flash, 2000);
+  };
+  flash();
+  window.setInterval(flash, 2000);
+})();
